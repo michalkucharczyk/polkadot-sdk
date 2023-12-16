33c7b5efd1ce5d1025677799c46de213c56a6223
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2023-12-16 12:14:12 +0100

    helper (#2)
    * wip: sp-core::core
    
    * wip: sp-keyring
    
    * wip: test-runtime
    
    * wip: keyring 2
    
    * wip: core 2
    
    * wip: application-crypto 2
    
    * fixes
    
    * substrate-bip39 bumped to 0.4.5
    
    * use Mnemonic::parse_in_normalized in no-std
    
    * Cargo.lock updated
    
    * keyring: features fix
    
    * ".git/.scripts/commands/fmt/fmt.sh"
    
    * bandersnatch fixes
    
    * compile feature removed
    
    * keyring: fixes
    
    * Cargo.lock
    
    * keyring: sp_std cleanup
    
    * debug added
    
    * unicode-normalization also supported in no-std
    
    * use fork of rust-bip39
    
    * Cargo.lock updated
    
    * more fixes
    
    * address_uri support in no-std, no-full-crypto
    
    * verify available in non-full-crypto
    
    * application-crypto: app_crypto_pair_common added
    
    * cargo.lock
    
    * Update substrate/primitives/core/Cargo.toml
    
    Co-authored-by: Davide Galassi <davxy@datawok.net>
    
    * Update substrate/frame/support/procedural/Cargo.toml
    
    Co-authored-by: Davide Galassi <davxy@datawok.net>
    
    * Update substrate/test-utils/runtime/Cargo.toml
    
    Co-authored-by: Davide Galassi <davxy@datawok.net>
    
    * proc-macro-crate version reverted
    
    * zepter fix
    
    * CI features checks added
    
    * Cargo.lock updated
    
    * make taplo happy
    
    * GenesisBuilder: new function
    
    * rococo: some initial thing
    
    * sc-chain-spec: some ground work
    
    * chain-spec: genesis-builder improvements
    
    * test-runtime: patch in runtime
    
    * sc-chain-spec: testing
    
    * test-runtime: dev
    
    * sc-chain-spec: EHF + named patch fixes
    
    * test-runtime: testing
    
    * test-runtime: testing
    
    * sp-genesis-builder: APIv2
    
    * test-runtime: genesis-builder api v2
    
    * build_condfig -> build_state
    
    * sp-genesis-builder: APIv2 c.d.
    
    * genesis-builder-helper: get_preset added
    
    * test-runtime: tests fixed
    
    * kitchensink: genesis-builder fixes
    
    * sc-chain-spec: support for genesis-builder v2
    
    * sc-chain-spec: little steps
    
    * chain-spec-builder: presets added
    
    * sp-genesis-builder: some doc added
    
    * doc fixed
    
    * Revert "rococo: some initial thing"
    
    This reverts commit 262bd70bcd73fbcb1fd5f709823f1a511a61f14e.
    
    * test-runtime: fix
    
    * rococo-runtime: presets added
    
    * Cargo.lock updated
    
    * naming: named_patch -> named_preset
    
    * naming: patch_name -> preset_name
    
    * fmt
    
    * preset_names/get_preset implemented for runtimes
    
    * polkadot-service: rococo-based chain-spec fixes
    
    * String -> &str
    
    * fmt
    
    * fixes
    
    * Cargo.lock updated
    
    ---------
    
    Co-authored-by: command-bot <>
    Co-authored-by: Davide Galassi <davxy@datawok.net>

--- polkadot/runtime/rococo/src/lib.rs
+++ polkadot/runtime/rococo/src/lib.rs
@@ -39,9 +39,6 @@ use runtime_common::{
 	paras_registrar, paras_sudo_wrapper, prod_or_fast, slots, BlockHashCount, BlockLength,
 	SlowAdjustingFeeUpdate,
 };
-use scale_info::TypeInfo;
-use sp_std::{cmp::Ordering, collections::btree_map::BTreeMap, prelude::*};
-
 use runtime_parachains::{
 	assigner as parachains_assigner, assigner_on_demand as parachains_assigner_on_demand,
 	assigner_parachains as parachains_assigner_parachains,
@@ -57,6 +54,9 @@ use runtime_parachains::{
 	scheduler as parachains_scheduler, session_info as parachains_session_info,
 	shared as parachains_shared,
 };
+use scale_info::TypeInfo;
+use sp_runtime::RuntimeString;
+use sp_std::{cmp::Ordering, collections::btree_map::BTreeMap, prelude::*};
 
 use authority_discovery_primitives::AuthorityId as AuthorityDiscoveryId;
 use beefy_primitives::{
@@ -66,7 +66,7 @@ use beefy_primitives::{
 
 use frame_support::{
 	construct_runtime, derive_impl,
-	genesis_builder_helper::{build_config, create_default_config},
+	genesis_builder_helper::{build_state, create_default_config},
 	parameter_types,
 	traits::{
 		fungible::HoldConsideration, Contains, EitherOf, EitherOfDiverse, EverythingBut,
@@ -128,6 +128,7 @@ use governance::{
 #[cfg(test)]
 mod tests;
 
+mod genesis_configs;
 mod validator_manager;
 
 impl_runtime_weights!(rococo_runtime_constants);
@@ -2395,8 +2396,24 @@ sp_api::impl_runtime_apis! {
 			create_default_config::<RuntimeGenesisConfig>()
 		}
 
-		fn build_config(config: Vec<u8>) -> sp_genesis_builder::Result {
-			build_config::<RuntimeGenesisConfig>(config)
+		fn build_state(config: Vec<u8>) -> sp_genesis_builder::Result {
+			build_state::<RuntimeGenesisConfig>(config)
+		}
+
+		fn get_preset(id: Option<Vec<u8>>) -> Option<Vec<u8>> {
+			if let Some(id) = id {
+				genesis_configs::get_preset(id)
+			} else {
+				Some(create_default_config::<RuntimeGenesisConfig>())
+			}
+		}
+
+		fn preset_names() -> Vec<sp_runtime::RuntimeString> {
+			vec![
+				RuntimeString::from("local_testnet"),
+				RuntimeString::from("development"),
+				RuntimeString::from("staging_testnet"),
+			]
 		}
 	}
 }
