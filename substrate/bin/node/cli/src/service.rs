5c217cebd38cc550d08b91ce1db916ebd6d94a34
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2024-05-23 13:34:57 +0200

    txpool builder: integrated

--- substrate/bin/node/cli/src/service.rs
+++ substrate/bin/node/cli/src/service.rs
@@ -175,7 +175,11 @@ pub fn new_partial(
 		FullBackend,
 		FullSelectChain,
 		sc_consensus::DefaultImportQueue<Block>,
+<<<<<<< HEAD
 		sc_transaction_pool::TransactionPoolImpl<Block, FullClient>,
+=======
+		sc_transaction_pool::TransactionPoolImpl<FullClient, Block>,
+>>>>>>> aa5d25adcdf (txpool builder: integrated)
 		(
 			impl Fn(
 				node_rpc::DenyUnsafe,
@@ -388,7 +392,11 @@ pub struct NewFullBase {
 	/// The syncing service of the node.
 	pub sync: Arc<SyncingService<Block>>,
 	/// The transaction pool of the node.
+<<<<<<< HEAD
 	pub transaction_pool: Arc<TransactionPoolImpl<Block, FullClient>>,
+=======
+	pub transaction_pool: Arc<TransactionPoolImpl<FullClient, Block>>,
+>>>>>>> aa5d25adcdf (txpool builder: integrated)
 	/// The rpc handlers of the node.
 	pub rpc_handlers: RpcHandlers,
 }
