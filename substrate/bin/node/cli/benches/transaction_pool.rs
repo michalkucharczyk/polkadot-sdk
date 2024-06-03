3a54152544b510ba5747e9e54e40ee69fe4f48c9
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2024-06-03 21:07:04 +0200

    clippy happy

--- substrate/bin/node/cli/benches/transaction_pool.rs
+++ substrate/bin/node/cli/benches/transaction_pool.rs
@@ -16,6 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+use polkadot_sdk::*;
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion, Throughput};
 use futures::{future, StreamExt};
 use kitchensink_runtime::{constants::currency::*, BalancesCall, SudoCall};
